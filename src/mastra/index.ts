b66d89a210cd3b5b2c6a2f0bb833ef16467a4374
somyatest1234-del <somyatest1234@gmail.com>
2025-10-27 16:02:36 +0530

    index.ts

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,18 +1,33 @@
-import { Mastra } from '@mastra/core/mastra';
-import { LibSQLStore } from '@mastra/libsql';
-import { PinoLogger } from '@mastra/loggers';
-import { codingAgent } from './agents/coding-agent';
+import { Mastra } from "@mastra/core/mastra";
+import { LibSQLStore } from "@mastra/libsql";
+import { PinoLogger } from "@mastra/loggers";
+import { codingAgent } from "./agents/coding-agent";
 
 export const mastra = new Mastra({
   agents: { codingAgent },
-  storage: new LibSQLStore({ url: 'file:../../mastra.db' }),
+  storage: new LibSQLStore({ url: "file:../.mastra.db" }),
   logger: new PinoLogger({
-    name: 'Mastra',
-    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    name: "Mastra",
+    level: process.env.NODE_ENV === "production" ? "info" : "debug",
   }),
   observability: {
     default: {
       enabled: true,
     },
+  },
+
+  mcp: {
+    enabled: true,
+    servers: [
+      {
+        name: "CData Managed",
+        type: "http",
+        url: "https://mcp.cloud.cdata.com/mcp",
+        auth: {
+          type: "bearer",
+          token: process.env.CDATA_API_TOKEN, // Stored securely in Mastra Env Variables
+        },
+      },
+    ],
   },
 });
